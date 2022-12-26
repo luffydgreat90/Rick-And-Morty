128c43982ad552e8a70402080eedcd012ff2d679
marlon ansale <luffydgreat90@gmail.com>
2022-12-26 20:13:09 +0000

    Added Feed UI.

--- RickAndMorty/RickAndMorty/SceneDelegate.swift
+++ RickAndMorty/RickAndMorty/SceneDelegate.swift
@@ -18,9 +18,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
     }
-
-
-
-
 }
-
