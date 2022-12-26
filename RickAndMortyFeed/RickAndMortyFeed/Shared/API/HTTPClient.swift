b4100e4d070d9434c1a0dd8a20f5f7d81d9c019d
marlon ansale <luffydgreat90@gmail.com>
2022-12-26 18:58:25 +0000

    Added `URLSessionHTTPClientTests`.

--- RickAndMortyFeed/RickAndMortyFeed/Shared/API/HTTPClient.swift
+++ RickAndMortyFeed/RickAndMortyFeed/Shared/API/HTTPClient.swift
@@ -16,5 +16,6 @@ public protocol HTTPClient {
     
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to dispatch to appropriate threads, if needed.
+    @discardableResult
     func get(from url: URL, completion: @escaping (Result) -> Void) -> HTTPClientTask
 }
