fbffb9210f5e7a1765d3794e729e78c53475f4e0
marlon ansale <luffydgreat90@gmail.com>
2022-12-27 09:00:51 +0000

    Added String.

---
+++ RickAndMortyFeed/RickAndMortyiOS/Feed UI/Controllers/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  RickAndMortyFeed
+//
+//  Created by Marlon Ansale on 12/26/22.
+//
+
+import UIKit
+
+public final class FeedViewController: UITableViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
