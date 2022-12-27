267fd437c365d67e794f14709892bb17e9c72766
marlon ansale <luffydgreat90@gmail.com>
2022-12-27 17:07:24 +0000

    Fix issue text.

--- RickAndMortyFeed/RickAndMortyFeed/FeedCharacter/Presentation/FeedCharacterPresenter.swift
+++ RickAndMortyFeed/RickAndMortyFeed/FeedCharacter/Presentation/FeedCharacterPresenter.swift
@@ -11,7 +11,7 @@ public final class FeedCharacterPresenter {
     public static var title: String {
         NSLocalizedString(
             "FEED_TITLE",
-            tableName: "Feed",
+            tableName: "FeedCharacter",
             bundle: Bundle(for: FeedCharacterPresenter.self),
             comment: "Title for the feed view")
     }
