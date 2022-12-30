90ef9c231284d64eec779727689eb17ecc991a6f
marlon ansale <luffydgreat90@gmail.com>
2022-12-30 23:42:54 +0000

    Added `CharacterViewController`.

---
+++ RickAndMortyFeed/RickAndMortyiOS/Character UI/Controllers/CharacterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterViewController.swift
+//  RickAndMortyiOS
+//
+//  Created by Marlon Ansale on 12/30/22.
+//
+
+import UIKit
+import RickAndMortyFeed
+
+public final class CharacterViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
