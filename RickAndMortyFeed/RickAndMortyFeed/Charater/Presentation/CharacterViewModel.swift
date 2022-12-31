70a528443ed59fd783f8aebe325d0246bb2f9d5f
marlon ansale <luffydgreat90@gmail.com>
2022-12-31 15:44:04 +0000

    Added Character.

---
+++ RickAndMortyFeed/RickAndMortyFeed/Charater/Presentation/CharacterViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterViewModel.swift
+//  RickAndMortyFeed
+//
+//  Created by Marlon Ansale on 12/31/22.
+//
+
+import Foundation
+
+public struct CharacterViewModel {
+    let title: String
+    let status: String
+}
